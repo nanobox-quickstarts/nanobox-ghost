e89fbbba594d162aac74e42bcc23be2002eb7cfa
Andy Eskridge <andy.eskridge@gmail.com>
2017-07-25 22:26:58 -0500

    upgrade to ghost 1.0.0

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-var path = require('path');
+var config = require('./node_modules/ghost/core/server/config'),
+    ghostVersion = require('./node_modules/ghost/core/server/utils/ghost-version');
+
+require('./node_modules/ghost/core/server/overrides');
+
+config.set('database:connection:host', process.env.DATA_DB_HOST);
+config.set('database:connection:user', process.env.DATA_DB_USER);
+config.set('database:connection:password', process.env.DATA_DB_PASS);
+
 var ghost = require('ghost');
-ghost({
-  config: path.join(__dirname, 'config.js')
-}).then(function (ghostServer) {
-  ghostServer.start();
-})
+var path = require('path');
+
+ghost().then(function (ghostServer) {
+    ghostServer.start();
+});
