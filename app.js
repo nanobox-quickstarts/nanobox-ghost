58c02d704f4d62a45f89c4a24d8f71d6593230e6
Andy Eskridge <andy.eskridge@gmail.com>
2017-07-26 08:48:45 -0500

    prepare for pull request

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ var config = require('./node_modules/ghost/core/server/config'),
 
 require('./node_modules/ghost/core/server/overrides');
 
+
+//since the config files are now json instead of js, this is the easiest
+//way to set the config from environment variables.
 config.set('database:connection:host', process.env.DATA_DB_HOST);
 config.set('database:connection:user', process.env.DATA_DB_USER);
 config.set('database:connection:password', process.env.DATA_DB_PASS);
 
+//these two environment variables need to be created on the dashboard, 
+//or via `nanobox add Name Value` 
+//this also assumes that you are using Mailgun as the provider
 config.set('mail:options:auth:user', process.env.MAIL__OPTIONS__AUTH__USER);
 config.set('mail:options:auth:pass', process.env.MAIL__OPTIONS__AUTH__PASS);
 
